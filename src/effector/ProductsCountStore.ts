bf0faa6eef39873cc4bcc764b67f7824092907e5
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-03-29 13:01:48 +0500

    Add localStorage

--- src/effector/ProductsCountStore.ts
+++ src/effector/ProductsCountStore.ts
@@ -1,11 +1,18 @@
 import {createEvent, createStore, sample} from "effector";
 import {AddNewProduct} from "./ProductsStore";
 
-export const $NewProductId = createStore<number>(0);
+const defaultNewProductId:number = window.localStorage.getItem("newProductId") === null
+    ? 0 // @ts-ignore
+    : Number(JSON.parse(window.localStorage.getItem("newProductId")));
+export const $NewProductId = createStore<number>(defaultNewProductId);
 const Increment = createEvent<void>("IncrementProductCount");
 
 $NewProductId
-    .on(Increment, state => state + 1)
+    .on(Increment, state => {
+        const newState = state + 1;
+        window.localStorage.setItem("newProductId", JSON.stringify(newState));
+        return newState;
+    })
     .watch(count => console.log("Products Counter:", count, "\n\n"));
 
 sample({
--- src/effector/ProductsStore.ts
+++ src/effector/ProductsStore.ts
@@ -1,6 +1,8 @@
 import {createEvent, createStore, sample} from "effector";
 import {EditProductType, ProductType, ShopType} from "../types/types";
 
+// TODO: Сергей Кашкин: Обернуть все watch'еры в process.env.NODE_ENV === 'development'
+
 export const AddNewProduct = createEvent<ProductType>("AddNewProduct");
 AddNewProduct.watch(product => console.log('Добавлен продукт:', product, "\n\n"));
 
@@ -13,19 +15,29 @@ DeleteProduct.watch(productId => console.log('Удалён продукт c id:'
 export const EditProduct = createEvent<EditProductType>("EditProduct");
 EditProduct.watch(product => console.log('Добавлен продукт:', product, "\n\n"));
 
-const $store = createStore<ProductType[]>([]);
+const defaultState = window.localStorage.getItem("store") === null
+    ? [] // @ts-ignore
+    : JSON.parse(window.localStorage.getItem("store"));
+const $store = createStore<ProductType[]>(defaultState);
 
 $store
-    .on(AddNewProduct, (state, product: ProductType) => [...state, product])
+    .on(AddNewProduct, (state, product: ProductType) => {
+        const newState = [...state, product]
+        window.localStorage.setItem("store", JSON.stringify(newState));
+        return newState;
+    })
     .on(BuyingProduct, (state, productId: number) => {
         const newState = state.slice();
         const product = newState.find(product => product.id === productId)
         if (!!product)
             product.bought = !product.bought;
+        window.localStorage.setItem("store", JSON.stringify(newState));
         return newState;
     })
     .on(DeleteProduct, (state, productId: number) => {
-        return state.filter(product => product.id !== productId);
+        const newState = state.filter(product => product.id !== productId)
+        window.localStorage.setItem("store", JSON.stringify(newState));
+        return newState;
     })
     .on(EditProduct, (state, newProduct: EditProductType) => {
         const newState = state.slice();
@@ -38,6 +50,7 @@ $store
                 newState[productIndex][field] = payload[field];
             }
         }
+        window.localStorage.setItem("store", JSON.stringify(newState));
         return newState;
     })
     .watch(products => console.log("Весь Store:", products, "\n\n"));
@@ -45,20 +58,32 @@ $store
 export const ChangeFilter = createEvent<ShopType[]>("ChangeFilter");
 const ApplyFilters = createEvent<{state: ProductType[], filters: ShopType[]}>("ApplyFilters");
 
-export const $products = createStore<ProductType[]>($store.defaultState);
-const $activeFilters = createStore<ShopType[]>([]);
+const defaultProducts = window.localStorage.getItem("products") === null
+    ? [] // @ts-ignore
+    : JSON.parse(window.localStorage.getItem("products"));
+export const $products = createStore<ProductType[]>(defaultProducts);
+
+const defaultFilters:ShopType[] = window.localStorage.getItem("filters") === null
+    ? [] // @ts-ignore
+    : JSON.parse(window.localStorage.getItem("filters"));
+const $activeFilters = createStore<ShopType[]>(defaultFilters);
 
 $activeFilters
-    .on(ChangeFilter, (state, newFilters) => newFilters)
+    .on(ChangeFilter, (state, newFilters) => {
+        window.localStorage.setItem("filters", JSON.stringify(newFilters));
+        return newFilters;
+    })
     .watch(filters => console.log('Фильтры:', filters, '\n\n'));
 
 const CheckAllFilter = (product:ProductType, filters:ShopType[]) => !!product.shop && filters.includes(product.shop);
 
 $products
     .on(ApplyFilters, (_, newState) => {
-        return newState.filters.length
+        const products = newState.filters.length
             ? newState.state.filter(product => CheckAllFilter(product, newState.filters))
             : newState.state
+        window.localStorage.setItem("products", JSON.stringify(products));
+        return products;
     })
     .watch(products => console.log("Отфильтрованный Store:", products, '\n\n'));
 
