3338b3d9fc2fcc9d994ec1288f4ef1ba91eae8bb
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-03-31 17:49:19 +0500

    Some refactor and comment

--- src/TSV.ts
+++ src/TSV.ts
@@ -1,9 +1,11 @@
 import {ProductType} from "./types/types";
 
+// const productsStore = useStore($products);
+// <a href={URL.createObjectURL(TSV(productsStore))} download={true}>TSV</a> загрузка файла в компоненте.
 export const TSV = (products: ProductType[]) => {
     const keys = new Set();
     if (products.length) {
-        const lines: BlobPart[] = [];
+        const lines = [];
         products.forEach(product => {
             const result = [];
             for (const key in product)
@@ -16,7 +18,7 @@ export const TSV = (products: ProductType[]) => {
             lines.push('\r\n');
             lines.push(result.join('\t'));
         });
-        lines.unshift(Array.from(keys).join('\t'))
+        lines.unshift(Array.from(keys).join('\t'));
         return new File(lines, 'tsv.tsv');
     }
     return new File([], 'tsv.tsv');
