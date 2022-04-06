edca9ae6bb9641c5e33adfab830617ceb15c2137
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-06 11:28:47 +0500

    Add watcher for states and events

--- src/models/init.ts
+++ src/models/init.ts
@@ -1,3 +1,20 @@
 import './allProducts/init';
 import './filteredProducts/init';
+import {Store, Event} from "effector";
+import {$store, AddNewProduct, BuyingProduct, DeleteProduct, EditProduct} from "./allProducts/ProductsStore";
+import {$activeFilters, $products} from "./filteredProducts/FilteredProductStore";
+import {$NewProductId} from "./allProducts/ProductsCountStore";
 
+const watcher = (message: string, target:Store<any> | Event<any>) => {
+    if (process.env.NODE_ENV === 'development')
+        target.watch(value => console.log(message, value, "\n\n"));
+}
+
+watcher("Весь Store:", $store);
+watcher("Добавлен продукт:", AddNewProduct);
+watcher("Куплен продукт c id:", BuyingProduct);
+watcher("Удалён продукт c id:", DeleteProduct);
+watcher("Изменён продукт:", EditProduct);
+watcher("Фильтры:", $activeFilters);
+watcher("Отфильтрованный Store:", $products);
+watcher("Products Counter:", $NewProductId);
