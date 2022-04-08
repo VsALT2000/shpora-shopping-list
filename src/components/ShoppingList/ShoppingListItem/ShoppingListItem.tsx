9d6fe68aadc0ea15a2ac6043abc5d0d405e8b25a
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-08 23:27:48 +0500

    Add animation for kebab menu

--- src/components/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
+++ src/components/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
@@ -18,29 +18,40 @@ export const ShoppingListItem: React.FC<ProductType> = (props) => {
         setPurchasedProduct(true)
     }
 
+    const dateOption = {
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric'
+    };
+    // @ts-ignore
+    const date = new Intl.DateTimeFormat('ru', dateOption).format(props.date)
     const options = `${styles.shoppingListItemOptions} ${closedOptions ? styles.closedOptions : styles.openedOptions}`;
     return (
-        <div className={styles.shoppingListItem}>
-            <input type="checkbox" defaultChecked={purchasedProduct} onChange={purchaseProductHandler}/>
-            <div className={styles.shoppingListItemContent} onClick={contentClickHandler}>
-                <label>{props.name}</label>
-                {closedOptions && (
-                    <span>
+        <div className={styles.itemWrapper}>
+            <div className={styles.itemContentLeftPart}>
+                <input type="checkbox" defaultChecked={purchasedProduct} onChange={purchaseProductHandler}/>
+                <div className={styles.shoppingListItemContent} onClick={contentClickHandler}>
+                    <label>{props.name}</label>
+                    {closedOptions && (
+                        <span>
                         | {props.amount}{props.unit}{props.price && ` ${props.price * props.amount}₽`}
                     </span>
-                )}
-                <svg className={`${styles.arrow} ${!closedOptions && styles.arrowReverse}`} width="12" height="8"
-                     viewBox="0 0 12 8" fill="none">
-                    <path d="M11 1.75L6 7L0.999999 1.75" stroke="#8d8d8d" strokeWidth="2" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                </svg>
-                <div>
-                    <p className={options}>Количество: {props.amount}</p>
-                    {props.shop && <p className={options}>Магазин: {props.shop}</p>}
-                    {props.price && <p className={options}>Цена: {props.price}₽</p>}
+                    )}
+                    <svg className={`${styles.arrow} ${!closedOptions && styles.arrowReverse}`} width="12" height="8"
+                         viewBox="0 0 12 8" fill="none">
+                        <path d="M11 1.75L6 7L0.999999 1.75" stroke="#8d8d8d" strokeWidth="2" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                    </svg>
+                    <div>
+                        <p className={options}>Количество: {props.amount}</p>
+                        <p className={options}>Дата добавления: {date}</p>
+                        {props.shop && <p className={options}>Магазин: {props.shop}</p>}
+                        {props.price && <p className={options}>Цена: {props.price}₽</p>}
+                    </div>
                 </div>
             </div>
-            <Actions product={props}/>
+            <div>
+                <Actions product={props}/>
+            </div>
         </div>
     );
 };
