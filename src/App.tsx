0e55e2cf3faf054cbf1f3f8f18bb8162fcd5d575
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-11 21:26:20 +0500

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,38 @@ import React, {useState} from 'react';
 import {ShoppingList} from './components/ShoppingList/ShoppingList';
 import classes from './App.less';
 import {EditItemForm} from "./components/EdiItemForm/EditItemForm";
+import {Routes, Route, useParams} from 'react-router-dom';
+
+const Test = () => {
+    return (
+        <div>
+            Nothing is here. Type on url /shpora-shopping-list/0
+        </div>
+    );
+}
+
+const ProductList = () => {
+    const ShoppingListId = useParams().ShoppingListId;
+    const [openedForm, setOpenedForm] = useState(false);
+    return (
+        <div>
+            <ShoppingList onOpenForm={() => setOpenedForm(true)}/>
+            {openedForm && <EditItemForm onCloseForm={() => setOpenedForm(false)}/>}
+        </div>
+    );
+}
 
 const App: React.FC = () => {
-    const [openedForm, setOpenedForm] = useState(false);
 
     return (
         <div className={classes.appWrapper}>
             <div className={classes.appContainer}>
-                <ShoppingList onOpenForm={() => setOpenedForm(true)}/>
-                {openedForm && <EditItemForm onCloseForm={() => setOpenedForm(false)}/>}
+                <Routes>
+                    <Route path={"/"}>
+                        <Route path={":ShoppingListId"} element={<ProductList/>}/>
+                        <Route index element={<Test/>}/>
+                    </Route>
+                </Routes>
             </div>
         </div>
     );
