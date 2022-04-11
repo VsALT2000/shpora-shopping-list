2faded1f0bfa8cb52abd17b57e50485dd13e35a9
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-11 11:18:55 +0500

    Added spread props for Button

--- src/components/Common/FormControl/Button.tsx
+++ src/components/Common/FormControl/Button.tsx
@@ -3,6 +3,6 @@ import styles from './Button.less'
 
 export const Button: React.FC<React.HTMLProps<HTMLButtonElement>> = (props) => {
     return (
-        <button type='submit' className={styles.button} onClick={props.onClick}>{props.children}</button>
+        <button className={styles.button} {...props} type='submit'>{props.children}</button>
     )
 }
