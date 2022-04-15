5a11aa7508530abab3f8be1803c69baef04bd64d
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-15 16:44:54 +0500

    Added cn in Checkbox

--- src/components/Common/FormControl/Checkbox.tsx
+++ src/components/Common/FormControl/Checkbox.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Checkbox.less';
+import cn from "classnames";
 
 const Checkbox: React.FC<React.HTMLProps<HTMLInputElement>> = (props) => {
     return (
-        <input className={styles.Checkbox} {...props} type="checkbox"/>
+        <input {...props} className={cn(styles.Checkbox, {[props.className as string]: Boolean(props.className)})} type="checkbox"/>
     );
 };
 
