173a82334ed1299cff36fa787519134ba87ee8ae
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-21 13:19:09 +0500

    Refactored

--- src/models/productsList/init.ts
+++ src/models/productsList/init.ts
@@ -1,17 +1,7 @@
 import {sample} from "effector";
-import {$listsStore, AddNewList, AddProductToList, DeleteList, DeleteProductFromList} from "./ProductsListStore";
+import {AddNewList, AddProductToList, DeleteList, DeleteProductFromList} from "./ProductsListStore";
 import {$NewListId, Increment} from "./ProductsListCountStore";
 import {AddNewProduct, DeleteProducts} from "../allProducts/ProductsStore";
-import {ProductsListType} from "../../types/types";
-
-const getList = (sourceData: ProductsListType[], clockData: number) => {
-    const pendingProducts = sourceData.find(list => list.id === clockData)?.pendingProducts || [];
-    const boughtProducts = sourceData.find(list => list.id === clockData)?.boughtProducts || [];
-    console.log(sourceData)
-    console.log(pendingProducts)
-    console.log(boughtProducts)
-    return [...pendingProducts, ...boughtProducts];
-}
 
 sample({
     clock: AddNewList,
