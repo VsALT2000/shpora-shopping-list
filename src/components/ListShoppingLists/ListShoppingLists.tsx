7699eb12a20431021a5e4a56a54304f82b065c7d
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-21 15:43:16 +0500

    Added TSV in ListShoppingLists (without filter and sort)

--- src/components/ListShoppingLists/ListShoppingLists.tsx
+++ src/components/ListShoppingLists/ListShoppingLists.tsx
@@ -6,11 +6,14 @@ import actionStyles from "../ShoppingList/ShoppingListItem/Actions/Actions.less"
 import AddNewItemButton from "../Common/FormControl/AddNewItemButton";
 import {$listsStore, DeleteList} from "../../models/productsList/ProductsListStore";
 import {useStore} from "effector-react";
-import {ProductsListType} from "../../types/types";
+import {ProductsListType, ProductType} from "../../types/types";
 import AddListInput from "../Common/FormControl/AddListInput";
 import cn from "classnames";
+import {TSV} from "../../TSV";
+import {$store} from "../../models/allProducts/ProductsStore";
 
 const ItemListShoppingLists: React.FC<ProductsListType> = (props) => {
+    const products = useStore($store);
     const navigate = useNavigate();
     const [openedKebab, setOpenedKebab] = useState(true);
 
@@ -25,6 +28,9 @@ const ItemListShoppingLists: React.FC<ProductsListType> = (props) => {
         setOpenedKebab(false);
         window.addEventListener("click", closeKebab);
     };
+
+    let pendingProducts = props.pendingProducts.map((id) => products.find((product) => product.id === id) as ProductType);
+    let boughtProducts = props.boughtProducts.map((id) => products.find((product) => product.id === id) as ProductType);
 
     return (
         <div className={styles.itemWrapper}>
@@ -42,7 +48,9 @@ const ItemListShoppingLists: React.FC<ProductsListType> = (props) => {
                         <div
                             className={cn(actionStyles.blueIcon, {[actionStyles.openedOptions]: !openedKebab})}
                             onClick={() => console.log("Тут будет скачивание TSV")}>
-                            <DownloadIcon className={styles.icon}/>
+                            <a href={URL.createObjectURL(TSV([...pendingProducts, ...boughtProducts]))} download={true}>
+                                <DownloadIcon className={styles.icon}/>
+                            </a>
                         </div>
                         <div
                             className={cn(actionStyles.redIcon, {[actionStyles.openedOptions]: !openedKebab})}
